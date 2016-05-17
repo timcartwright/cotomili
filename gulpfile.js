b4c224c2a53f90b01544eaa5ea1b2228dc87a841
timcartwright <tim@websitesbytim.com>
2016-05-17 17:21:24 +0400

    Create production files with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,7 @@ gulp.task('compass', function() {
 // HTML
 gulp.task('html', function() {
 	gulp.src('builds/development/*.html')
-		.pipe(gulpif(env === 'production', jsonminify()))
+		.pipe(gulpif(env === 'production', minifyHTML()))
 		.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
 		.pipe(connect.reload()) // Server reload
 });
@@ -100,7 +100,7 @@ gulp.task('html', function() {
 // json
 gulp.task('json', function() {
 	gulp.src('builds/development/js/*.json')
-		.pipe(gulpif(env === 'production', minifyHTML()))
+		.pipe(gulpif(env === 'production', jsonminify()))
 		.pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
 		.pipe(connect.reload()) // Server reload
 });
